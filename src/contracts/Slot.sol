32c3ad5bd07725ad7cb5d74dbb0fde6769d53909
y0rgos <y0rgos@proton.me>
2022-10-29 01:21:44 +0300

    Add payouts on Slot.sol

--- src/contracts/Slot.sol
+++ src/contracts/Slot.sol
@@ -5,8 +5,11 @@ pragma solidity ^0.8;
 contract Slot {
 
     uint8[3] public global_result;
+    uint public global_payout;
+
     uint randNonce = 0;
     uint8[36] public reel;
+    mapping(uint8=>uint256[2]) payouts;
 
     constructor(){
         uint8[2][8] memory symbols = [[0,8],
@@ -17,6 +20,15 @@ contract Slot {
                                      [5,3],
                                      [6,2],
                                      [7,1]];
+
+        payouts[0] = [4 , 27];
+        payouts[1] = [6 , 43];
+        payouts[2] = [9 , 92];
+        payouts[3] = [14 , 240];
+        payouts[4] = [24 , 540];
+        payouts[5] = [46 , 1660];
+        payouts[6] = [170  , 6250];
+        payouts[7] = [1140 , 100000];
         
         uint8 reel_counter;
         for (uint8 symbol = 0; symbol < symbols.length; symbol++){
@@ -30,6 +42,13 @@ contract Slot {
     function spin() public returns(uint8[3] memory) {
         uint8[3] memory result = [reel[randMod(36)], reel[randMod(36)], reel[randMod(36)]];
         global_result = result;
+        if (result[0] == result[1] && result[1] == result[2]){
+            global_payout = (1 ether * payouts[result[0]][1]) / 10;
+        } else if (result[0] == result[1]){
+            global_payout = (1 ether * payouts[result[0]][0]) / 10;
+        } else {
+            global_payout = 0;
+        }
         return result;
     }
 
