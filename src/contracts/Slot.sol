71dc2e669f2f694dfbf8f40f183290ab76f56cd1
y0rgos <y0rgos@proton.me>
2022-10-30 22:03:38 +0200

    Fully functional slot with owing feature

--- src/contracts/Slot.sol
+++ src/contracts/Slot.sol
@@ -3,6 +3,9 @@
 pragma solidity ^0.8;
 
 contract Slot {
+
+    mapping(uint8 => uint[2]) payouts;
+    uint8[36] public reel;
     uint256[5] public validBetAmounts = [
         0.2 ether,
         0.4 ether,
@@ -11,9 +14,9 @@ contract Slot {
         1 ether
     ];
 
+    mapping(address => uint) public oweToAddress;
+
     uint256 randNonce = 0;
-    uint8[36] public reel;
-    mapping(uint8 => uint256[2]) payouts;
 
     constructor() {
         // The symbols on each reel (first place) and the number of occurrence (second place)
@@ -30,21 +33,23 @@ contract Slot {
 
         // Multiplier payouts of each combination, first place is x2, second place is x3
         // (multipliers are x*10 because solidity does not support float numbers and they get x/10 after bet*payout calculation)
-        payouts[0] = [4, 27];
-        payouts[1] = [6, 43];
-        payouts[2] = [9, 92];
-        payouts[3] = [14, 240];
-        payouts[4] = [24, 540];
-        payouts[5] = [46, 1660];
-        payouts[6] = [170, 6250];
-        payouts[7] = [1140, 100000];
+
+        // ~= 95% RTP
+        payouts[0] = [2, 25];
+        payouts[1] = [5, 45];
+        payouts[2] = [10, 100];
+        payouts[3] = [25, 250];
+        payouts[4] = [25, 550];
+        payouts[5] = [45, 1650];
+        payouts[6] = [150, 6550];
+        payouts[7] = [1150, 100000];
 
         // Populate the reels with symbols with their corresponding number of occurrence
         // (this slot has 3 same reels so we use 1 reel)
         uint8 reelCounter;
         for (uint8 symbol = 0; symbol < symbols.length; symbol++) {
             for (
-                uint256 symbolOccur = 0;
+                uint8 symbolOccur = 0;
                 symbolOccur < symbols[symbol][1];
                 symbolOccur++
             ) {
@@ -54,8 +59,10 @@ contract Slot {
         }
     }
 
+    receive() external payable {}
+
+
     // Spin the slot
-    // (not ready yet)
     function spin() public payable returns (uint8[3] memory, uint256) {
         // Check if the bet is valid
         bool validBet = false;
@@ -85,8 +92,34 @@ contract Slot {
             payout = (msg.value * payouts[result[0]][0]) / 10;
         }
 
+        if (payout > 0) {
+            if (int(address(this).balance) - int(payout) >= 0){
+                payable(msg.sender).transfer(payout);
+            } else {
+                oweToAddress[msg.sender] += payout;
+            }
+        }
+
         return (result, payout);
     }
+
+    function getOwedMoney() external {
+        require(oweToAddress[msg.sender] > 0, "Slot owes you nothing");
+        require(address(this).balance > 0, "Slot cannot pay you right now");
+
+        if (int(address(this).balance) - int(oweToAddress[msg.sender]) >= 0){
+            payable(msg.sender).transfer(oweToAddress[msg.sender]);
+            oweToAddress[msg.sender] = 0;
+        } else {
+            uint canAfford = oweToAddress[msg.sender] - address(this).balance;
+            payable(msg.sender).transfer(canAfford);
+            oweToAddress[msg.sender] -= canAfford;
+        }
+    }
+
+
+
+
 
     // Temporary pseudorandom function
     function randMod(uint256 _modulus) internal returns (uint8) {
--- src/scripts/deploy.py
+++ src/scripts/deploy.py
@@ -1,10 +1,29 @@
 from brownie import Slot, accounts
+from web3 import Web3
+
+owner_acc = accounts[0]
+player_acc = accounts[1]
 
 def main():
-    Slot.deploy({'from': accounts[0]})
+    Slot.deploy({'from': owner_acc })
     slot = Slot[-1]
-    while True:
-        input('Press Enter To Spin')
-        tx = slot.spin({'from': accounts[0], 'value': 10**18})
+    #owner_acc.transfer(slot, '10 ether')
+
+    bet = Web3.toWei(0.2, 'ether')
+
+    #player_acc.transfer(0x0, '980 ether')
+    while player_acc.balance() >= bet:
+        tx = slot.spin({'from': player_acc, 'value': bet})
         tx.wait(1)
-        print(tx.return_value)
+        print(tx.return_value[0])
+        print('Bet: ', Web3.fromWei(bet, 'ether'))
+        print('Slot balance: ', Web3.fromWei(slot.balance(), 'ether'))
+        print('Win: ', Web3.fromWei(tx.return_value[1], 'ether'))
+        print('Player balance: ', Web3.fromWei(player_acc.balance(), 'ether'))
+        print('Owes to player: ', Web3.fromWei(slot.oweToAddress(player_acc.address), 'ether'))
+        if Web3.fromWei(tx.return_value[1], 'ether') > 0:
+            inp = input('Press Enter To Spin')
+            if 'r' in inp:
+                slot.getOwedMoney({'from': player_acc})
+                print('Slot balance: ', Web3.fromWei(slot.balance(), 'ether'))
+                print('Player balance: ', Web3.fromWei(player_acc.balance(), 'ether'))
