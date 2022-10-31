c2826f18ffee6fe749cd76fe16c2edf8ae1570fe
y0rgos <y0rgos@proton.me>
2022-10-31 17:56:14 +0200

    Refactor code & fix bugs and vulnerabilities

--- src/contracts/Slot.sol
+++ src/contracts/Slot.sol
@@ -1,22 +1,12 @@
 //SPDX-License-Identifier: UNLICENSED
 
-pragma solidity ^0.8;
+pragma solidity 0.8.17;
 
 contract Slot {
-
-    mapping(uint8 => uint[2]) payouts;
     uint8[36] public reel;
-    uint256[5] public validBetAmounts = [
-        0.2 ether,
-        0.4 ether,
-        0.6 ether,
-        0.8 ether,
-        1 ether
-    ];
-
-    mapping(address => uint) public oweToAddress;
-
     uint256 randNonce = 0;
+    mapping(uint8 => uint256[2]) payouts;
+    mapping(address => uint256) public oweToAddress;
 
     constructor() {
         // The symbols on each reel (first place) and the number of occurrence (second place)
@@ -42,7 +32,7 @@ contract Slot {
         payouts[4] = [25, 550];
         payouts[5] = [45, 1650];
         payouts[6] = [150, 6550];
-        payouts[7] = [1150, 100000];
+        payouts[7] = [1150, 10e4];
 
         // Populate the reels with symbols with their corresponding number of occurrence
         // (this slot has 3 same reels so we use 1 reel)
@@ -61,20 +51,12 @@ contract Slot {
 
     receive() external payable {}
 
-
     // Spin the slot
     function spin() public payable returns (uint8[3] memory, uint256) {
-        // Check if the bet is valid
-        bool validBet = false;
-        for (uint256 i = 0; i < validBetAmounts.length; i++) {
-            if (msg.value == validBetAmounts[i]) {
-                validBet = true;
-                break;
-            }
-        }
+        // Check if the bet is in valid range(0.1 eth - 1 eth)
         require(
-            validBet,
-            "Bet is not valid. Valid amounts are 0.2, 0.4, 0.6, 0.8, 1.0 ether"
+            msg.value >= 0.1 ether && msg.value <= 1 ether,
+            "Bet is not valid (min: 0.1 ether, max: 1 ether)"
         );
 
         // Get 3 random results
@@ -85,7 +67,7 @@ contract Slot {
         ];
 
         // Check if player wins and calculate the reward
-        uint256 payout;
+        uint256 payout = 0;
         if (result[0] == result[1] && result[1] == result[2]) {
             payout = (msg.value * payouts[result[0]][1]) / 10;
         } else if (result[0] == result[1]) {
@@ -93,7 +75,7 @@ contract Slot {
         }
 
         if (payout > 0) {
-            if (int(address(this).balance) - int(payout) >= 0){
+            if (int256(address(this).balance) - int256(payout) >= 0) {
                 payable(msg.sender).transfer(payout);
             } else {
                 oweToAddress[msg.sender] += payout;
@@ -107,22 +89,22 @@ contract Slot {
         require(oweToAddress[msg.sender] > 0, "Slot owes you nothing");
         require(address(this).balance > 0, "Slot cannot pay you right now");
 
-        if (int(address(this).balance) - int(oweToAddress[msg.sender]) >= 0){
-            payable(msg.sender).transfer(oweToAddress[msg.sender]);
+        if (
+            int256(address(this).balance) - int256(oweToAddress[msg.sender]) >= 0
+        ) {
             oweToAddress[msg.sender] = 0;
+            payable(msg.sender).transfer(oweToAddress[msg.sender]);
         } else {
-            uint canAfford = oweToAddress[msg.sender] - address(this).balance;
-            payable(msg.sender).transfer(canAfford);
-            oweToAddress[msg.sender] -= canAfford;
+            uint256 newOweAmount = oweToAddress[msg.sender] -
+                address(this).balance;
+            oweToAddress[msg.sender] = newOweAmount;
+            payable(msg.sender).transfer(address(this).balance);
         }
     }
 
-
-
-
-
     // Temporary pseudorandom function
-    function randMod(uint256 _modulus) internal returns (uint8) {
+    // TODO: Replace it with Chainlink VRF
+    function randMod(uint256 modulus) internal returns (uint8) {
         randNonce++;
         return
             uint8(
@@ -130,7 +112,7 @@ contract Slot {
                     keccak256(
                         abi.encodePacked(block.timestamp, msg.sender, randNonce)
                     )
-                ) % _modulus
+                ) % modulus
             );
     }
 }
