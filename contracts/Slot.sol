31f8c6acf8f8c71b6ee469daa785c30e2ae6aacd
y0rgos <y0rgos@proton.me>
2022-11-15 19:44:12 +0200

    Add Chainlink VRF

--- contracts/Slot.sol
+++ contracts/Slot.sol
@@ -3,15 +3,60 @@
 
 pragma solidity 0.8.17;
 
-import "./PriceConsumerV3.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
+
 import "./BalanceManager.sol";
 
-contract Slot is BalanceManager, PriceConsumerV3 {
-    uint8[36] public reel;
-    uint256 randNonce = 0;
-    mapping(uint8 => uint256[2]) payouts;
+contract Slot is BalanceManager, VRFConsumerBaseV2, ConfirmedOwner {
+    //////////////////////////////////////// VRF //////////////////////////////////////
+    event RequestSent(uint256 requestId, uint32 numWords);
+    event RequestFulfilled(uint256 requestId, uint256[] randomWords);
 
-    constructor(address priceFeedAddress) PriceConsumerV3(priceFeedAddress) {
+    struct RequestStatus {
+        bool fulfilled; // whether the request has been successfully fulfilled
+        bool exists; // whether a requestId exists
+        uint256[] randomWords;
+        address from;
+        uint256 bet;
+    }
+    mapping(uint256 => RequestStatus) public s_requests; /* requestId --> requestStatus */
+    mapping(address => bool) public waitingForResponse;
+    VRFCoordinatorV2Interface COORDINATOR;
+
+    // Your subscription ID.
+    uint64 s_subscriptionId;
+
+    // past requests Id.
+    uint256[] public requestIds;
+    uint256 public lastRequestId;
+
+    bytes32 keyHash =
+        0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15;
+
+    uint32 callbackGasLimit = 3000000;
+
+    uint16 requestConfirmations = 3;
+
+    ///////////////////////////////////  Price Feed ///////////////////////////////////
+    AggregatorV3Interface internal priceFeed;
+    ///////////////////////////////////////////////////////////////////////////////////////
+
+
+    uint8[36] public reel;
+    mapping(uint256 => uint256[2]) payouts;
+
+    constructor(
+        address priceFeedAddress,
+        uint64 subscriptionId,
+        address vrfCoordinatorAddress
+    ) VRFConsumerBaseV2(vrfCoordinatorAddress) ConfirmedOwner(msg.sender) {
+        priceFeed = AggregatorV3Interface(priceFeedAddress);
+        COORDINATOR = VRFCoordinatorV2Interface(vrfCoordinatorAddress);
+        s_subscriptionId = subscriptionId;
+
         // The symbols on each reel and the number of occurrence
         // (layout : [symbol, occurrence])
         uint8[2][8] memory symbols = [
@@ -51,10 +96,9 @@ contract Slot is BalanceManager, PriceConsumerV3 {
     receive() external payable {}
 
     // Spin the slot
-    function spin(uint256 bet, uint256 spins)
+    function spin(uint256 bet, uint32 spins)
         public
         payable
-        returns (uint8[3] memory, uint256)
     {
         /* bet = USD value * Ether Unit (Example: 0.2 * 1e18 = 2e17 (or 200000000000000000))
         Example: bet = 2e17, spins = 50 =>
@@ -62,46 +106,88 @@ contract Slot is BalanceManager, PriceConsumerV3 {
         msg.value must be 1e19 or greater
         (1e19 = 10 USD in ether unit)
         */
+        require(!waitingForResponse[msg.sender], "Waiting For Response");
         uint256 price = uint256(getLatestPrice());
-        require(spins >= 1 && spins <= 100);
-        require(bet >= 0.1 ether && bet <= 1 ether);
+        require(spins >= 1 && spins <= 100, "1 <= Spins <= 100");
+        require(bet >= 0.1 ether && bet <= 1 ether, "$0.10 <= Bet <= $1.0");
         uint256 spinsValue = bet * spins;
         uint256 sentValue = (msg.value * price) / 1e8;
-        require(sentValue >= spinsValue);
+        require(sentValue >= spinsValue, "Insufficient Funds");
 
-        // Get 3 random results
-        uint8[3] memory result = [
-            reel[randMod(36)],
-            reel[randMod(36)],
-            reel[randMod(36)]
-        ];
-
-        uint256 payout = 0;
-
-        // Checks if the symbols on reel 1 and 2 are the same
-        if (result[0] == result[1]) {
-            // "pos" indicates on which position is the multiplier on "payouts" array
-            uint8 pos = 0;
-            // Checks if the symbols on reel 2 and 3 are the same and update pos to the corresponding position
-            if (result[1] == result[2]) pos = 1;
-            payout = (msg.value * payouts[result[0]][pos]) / 10;
-            updateBalance(msg.sender, payout);
-        }
-
-        return (result, payout);
+        waitingForResponse[msg.sender] = true;
+        requestRandomWords(bet, spins);
     }
 
-    // Temporary pseudorandom function
-    // TODO: Replace it with Chainlink VRF
-    function randMod(uint256 modulus) internal returns (uint8) {
-        randNonce++;
-        return
-            uint8(
-                uint256(
-                    keccak256(
-                        abi.encodePacked(block.timestamp, msg.sender, randNonce)
-                    )
-                ) % modulus
-            );
+    function getLatestPrice() public view returns (int256) {
+        (, int256 price, , , ) = priceFeed.latestRoundData();
+        return price;
+    }
+
+    // Assumes the subscription is funded sufficiently.
+    function requestRandomWords(uint256 bet, uint32 numWords)
+        private
+        returns (uint256 requestId)
+    {
+        // Will revert if subscription is not set and funded.
+        requestId = COORDINATOR.requestRandomWords(
+            keyHash,
+            s_subscriptionId,
+            requestConfirmations,
+            callbackGasLimit,
+            numWords
+        );
+        s_requests[requestId] = RequestStatus({
+            randomWords: new uint256[](0),
+            exists: true,
+            fulfilled: false,
+            from: msg.sender,
+            bet: bet
+        });
+        requestIds.push(requestId);
+        lastRequestId = requestId;
+        emit RequestSent(requestId, numWords);
+        return requestId;
+    }
+
+    function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        require(s_requests[_requestId].exists, "request not found");
+        s_requests[_requestId].fulfilled = true;
+        s_requests[_requestId].randomWords = _randomWords;
+        calculatePayout(s_requests[_requestId].bet, s_requests[_requestId].from, _randomWords);
+        waitingForResponse[s_requests[_requestId].from] = false;
+        emit RequestFulfilled(_requestId, _randomWords);
+    }
+
+    function getRequestStatus(uint256 _requestId)
+        external
+        view
+        returns (bool fulfilled, uint256[] memory randomWords)
+    {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    function calculatePayout(uint256 bet, address from, uint256[] memory randomWords) private {
+        for(uint8 i = 0; i < randomWords.length; i ++) {
+            uint256 r1 = reel[(randomWords[i] % 100) % 36];
+            uint256 r2 = reel[((randomWords[i] % 10000) / 100) % 36];
+            uint256 r3 = reel[((randomWords[i] % 1000000) / 10000) % 36];
+
+            uint256 payout = 0;
+
+            // Checks if the symbols on reel 1 and 2 are the same
+            if (r1 == r2) {
+                // "pos" indicates on which position is the multiplier on "payouts" array
+                uint8 pos = 0;
+                // Checks if the symbols on reel 2 and 3 are the same and update pos to the corresponding position
+                if (r2 == r3) pos = 1;
+                payout = (((bet * payouts[r1][pos]) / 10) * 1e8) / uint256(getLatestPrice());
+                updateBalance(from, payout);
+            }
+        }
     }
 }
--- scripts/deploy.py
+++ scripts/deploy.py
@@ -1,19 +1,36 @@
+from brownie.network import gas_price, show_active
+from brownie.network.gas.strategies import LinearScalingStrategy
 from brownie import (
     MockAggregatorV3Interface,
-    network,
+    VRFCoordinatorV2Mock,
+    LinkToken,
+    Slot,
     config,
     a)
-
-from .utils import eth_to_wei, wei_to_eth
 from .price_feed import get_price
+
+gas_strategy = LinearScalingStrategy("10 gwei", "50 gwei", 1.1)
 
 def main():
     deploy_all()
 
 
 def deploy_all():
-    if network.show_active() == 'development':
-        a[0].deploy(MockAggregatorV3Interface, get_price()[1])
-        price_feed_address = MockAggregatorV3Interface[-1].address
-    elif network.show_active() == 'goerli' or network.show_active() == 'mainnet-fork':
-        price_feed_address = config['networks'][network.show_active()]['price_feed_address']
+    if show_active() == 'development' or show_active() == 'dev':
+        price_feed_contract = MockAggregatorV3Interface.deploy(get_price()[1], {'from': a[0], 'gas_price': gas_strategy})
+        price_feed_address = price_feed_contract.address
+
+        link_token = LinkToken.deploy({'from': a[0], 'gas_price': gas_strategy})
+
+        vrf_coordinator = VRFCoordinatorV2Mock.deploy(1, 1, {'from': a[0], 'gas_price': gas_strategy})
+        vrf_coordinator.createSubscription({'from': a[0], 'gas_price': gas_strategy})
+        vrf_coordinator.fundSubscription(1, 100*1e18, {'from': a[0], 'gas_price': gas_strategy})
+        tx = vrf_coordinator.getSubscription(1)
+
+        balance, req_count, owner, consumers, = tx
+        print(f'{owner} funded {balance/1e18} LinkToken on sub 1')
+        print(req_count, consumers)
+
+        slot = Slot.deploy(price_feed_address, 1, vrf_coordinator.address, {'from': a[0],"gas_price": gas_strategy})
+        vrf_coordinator.addConsumer(1, slot.address, {'from': a[0],"gas_price": gas_strategy})
+
